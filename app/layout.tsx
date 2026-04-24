5c55f30e5a348f8e5699c11a3a12e628ab3e0e01
Muhammad Rehan <rehan2004aslam@gmail.com>
2026-04-24 14:55:33 +0500

    Netlify fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
     title: "QueueBuddy | Real-Time Gaming Lobbies",
     description: "Find players. Revive games. Play instantly. Join the ultimate server browser for Minecraft and more.",
     type: "website",
-    url: "https://queuebuddy.vercel.app",
+    url: "https://queuebuddy.netlify.app",
     images: [{ url: "/Queuebuddy.png" }],
   },
   verification: {
